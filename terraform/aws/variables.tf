83bfe707b7adbe397111b951bb71ec848c9d6899
thodson <thodson@usgs.gov>
2024-02-16 14:46:30 -0600

    Set jobmanager nodes to micro

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -48,7 +48,7 @@ variable "node_groups" {
     jobmanager = {
       node_group_name = "flink-jobmanager"
       max_instances = 10
-      instance_types = ["t3.large"]
+      instance_types = ["t3.micro"]
       capacity_type = "ON_DEMAND"
     },
     taskmanager = {
